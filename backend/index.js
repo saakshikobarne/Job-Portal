430b56b9e58e8dc3c683ed2b37b5b16733fbca5d
Saakshi Kobarne <saakshik15@gmail.com>
2024-10-05 15:53:58 +0530

    Set up MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,11 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
 
 const app= express();
-
-app.get('/home',(req,res)=>{
-    return res.status(200).json({
-        message: "I am coming from backend",
-        success: true
-    })
-})
 
 //middleware
 app.use(express.json());
@@ -21,7 +17,8 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
-const PORT= 3000
+const PORT= process.env.PORT || 3000;
 app.listen(PORT, ()=>{
     console.log(`server running at port: ${PORT}`);
+    connectDB();
 }) 
